efdb3c74751a82a8be096733608f1c467595d6ea
Jeanie Ma <jeanie.ma@gamesys.co.uk>
2020-08-17 09:34:40 +0100

    remove repeat selections

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-(function() {
+(function () {
     const wheel = document.querySelector('.wheel');
     const startButton = document.querySelector('.button');
     let deg = 0;
-
+    let currentWinningNumber = 0;
+    let winningOrder = [40, 80, 125, 170, 215, 260, 305, 340];
+    
+    function hasFallenIntoThatRange() {
+    currentWinningNumber = Math.floor(Math.random() * winningOrder.length);
+    landedNumber = winningOrder[currentWinningNumber];
+    console.log(winningOrder, currentWinningNumber, winningOrder[currentWinningNumber]);
+    winningOrder.splice(currentWinningNumber, 1);
+    if (winningOrder.length < 0) {
+    return;
+    }
+    return landedNumber;
+    }
+    
     startButton.addEventListener('click', () => {
-        startButton.getElementsByClassName.pointerEvents = 'none';
-        deg = Math.floor(5000 + Math.random() * 5000)
-        wheel.style.transition = 'all 10s ease-out';
-        wheel.style.transform = `rotate(${deg}deg)`;
-        wheel.classList.add('blur');
-
-    })
-
+    startButton.getElementsByClassName.pointerEvents = 'none';
+    deg = hasFallenIntoThatRange();
+    wheel.style.transition = 'all 1s ease-out';
+    wheel.style.transitionDuTration = '1s';
+    wheel.style.transform = `rotate(${deg}deg)`;
+    wheel.classList.add('blur');
+    });
+    
     wheel.addEventListener('transitionend', () => {
-        wheel.classList.remove('blur');
-        startButton.style.pointerEvents = 'auto';
-        wheel.style.transition = 'none';
-        const actualDeg = deg % 360;
-        wheel.style.transform = `rotate(${actualDeg}deg)`;
-    })
-})();
+    wheel.classList.remove('blur');
+    startButton.style.pointerEvents = 'auto';
+    wheel.style.transition = 'none';
+    const actualDeg = deg % 360;
+    wheel.style.transform = `rotate(${actualDeg}deg)`;
+    });
+   })();
